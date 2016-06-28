255cd456af188e43f23ed09d77aead74ab58ae53
David Martin <dwmartin41@gmail.com>
2016-06-28 22:37:24 +0100

    updated credentials to load from env vars if json file doesn't exist

--- project.clj
+++ project.clj
@@ -13,7 +13,8 @@
                  [clj-http "2.2.0"]
                  [cheshire "5.6.2"]
                  [clj-time "0.12.0"]
-                 [jerks-whistling-tunes "0.2.4"]]
+                 [jerks-whistling-tunes "0.2.4"]
+                 [environ "1.0.3"]]
   :main ^:skip-aot phonsole-server.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/phonsole_server/credentials.clj
+++ src/phonsole_server/credentials.clj
@@ -1,10 +1,21 @@
 (ns phonsole-server.credentials
   (:require [cheshire.core :refer [parse-string]]
-            [jerks-whistling-tunes.utils :refer [decode-base-64]]))
+            [jerks-whistling-tunes.utils :refer [decode-base-64]]
+            [clojure.java.io :as io]
+            [environ.core :refer [env]]))
 
 (defn get-auth0-credentials []
-  "loads the auth0 credentials from the auth0-credentials.json file (not in git)"
-  (-> (slurp "auth0-credentials.json")
-      (parse-string true)
-      (update :client-secret decode-base-64)
-      ))
+  "loads the auth0 credentials from the auth0-credentials.json file (not in git), or from env vars if the file does not exist"
+  (let [file (io/file "auth0-credentials.json")]
+    (-> (if (.exists file)
+          (-> (slurp file)
+              (parse-string true))
+          (let [keys [:client-id :client-secret :domain]]
+            (zipmap keys (map #(->> %
+                                    name
+                                    (str "auth0")
+                                    keyword
+                                    env)
+                              keys))))
+        (update :client-secret decode-base-64))
+    ))
